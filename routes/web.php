0570e2e15a9607009bd8108f639b0329ce719604
hogehoge.tarou <hogehogerakumma@gmail.com>
2018-06-19 06:46:16 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'TasksController@index');
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+Route::get('', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
